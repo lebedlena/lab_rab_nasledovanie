10fddef67e34e32d815ce6ec4b02e51c900fe626
Elena Lebed <111484478+lebedlena@users.noreply.github.com>
2022-08-29 17:42:44 +0300

    Update Aspirant.java

--- lab_rab_nasledovanie_Lebed EN/src/Aspirant.java
+++ lab_rab_nasledovanie_Lebed EN/src/Aspirant.java
@@ -4,13 +4,10 @@ public class Aspirant extends Student {
         super (firstName, lastName, String group);
         this.dissertation = dissertation;
     }
-    void Dissertation (){
-         int [] averageMark;
-                int sum = 0;
-                for (int i = 0; i < 5; i++) {
-                    sum += averageMark[i];}
-                
-                if (sum / 5 < 5) {
-                    System.out.println("- стипендия - 180 рублей");
+    @Override
+    void getScholarship (){
+            System.out.println("- стипендия - 180 рублей");
            else System.out.println("- стипендия - 200 рублей")}
+    }
+                 
 }
